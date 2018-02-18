678d2cca635fe677e6c20dd127b143615909d80d
Michal Svoboda <svoboda.michal@seznam.cz>
2018-02-18 17:38:19 +0100

    Data Processing: Clustering, tokenisation changed to quanteda library
    Prediction model predict_3:
    Speed up, limiting results, summing condprobs without grouping
    
    0.1554 s/predikce
    
    accuracy1 accuracy2 accuracy3 accuracy4 accuracy5
    1     0.448     0.546     0.608     0.632     0.674

--- data_processing.R
+++ data_processing.R
@@ -4,78 +4,65 @@ corp <- VCorpus(DirSource("./sample/en_US/",encoding = "UTF-8"))
 #corp <- VCorpus(DirSource("./sample2/en_US/",encoding = "UTF-8"))
 #corp <- VCorpus(DirSource("./test_set/",encoding = "UTF-8"))
 
-corp <- tm_map(corp,content_transformer(tolower)) # to lowercase | improve: Correction for names
-corp <- tm_map(corp,removePunctuation, ucp=TRUE) # remove punctuation (when ucp == False, ” are not removed)
-corp <- tm_map(corp,removeNumbers) # remove numbers
-corp <- tm_map(corp, content_transformer(function(x) {
+profanities<-read.table("profanities.txt",stringsAsFactors = F,strip.white = T,sep = "\n")[,1] #source: "https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/blob/master/en"
+skipWords <- function(x) removeWords(x,profanities)
+remPunct <- function(x) removePunctuation(x,ucp=TRUE)#  (when ucp == False, ” are not removed)
+removeNonWesternCharacters <- content_transformer(function(x) {
   id <- !is.na(iconv(x, from = "UTF-8", to = "WINDOWS-1252")) 
   print(paste("removed",as.character(length(x)-sum(id)),"lines"))
   x[id]
-})) # removal of lines with non-Western characters 
-corp <- tm_map(corp,stripWhitespace) # remove extra whitespace
-#profanities<-read.table("profanities.txt",stringsAsFactors = F)[,1] #source: "https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"
-#tm_map(corp,removeWords,words=profanities) #SLOW!
+}) # removal of lines with non-Western characters 
 
-# do clustering - 1-gram, 2-gram, 3-gram using weka 
-# clusters appearing just once will be ignored
+funs <- list(skipWords,
+             stripWhitespace,
+             remPunct,
+             removeNumbers,
+             content_transformer(tolower),
+             removeNonWesternCharacters)
+
+corp <- tm_map(corp,FUN=tm_reduce,tmFuns=funs)
+flatcorp <- c(corp[[1]][[1]],corp[[2]][[1]],corp[[3]][[1]])#make just a char vector by appending document contents
+
+library(quanteda)
 library(RWeka)
-gr1 <- TermDocumentMatrix(corp,control = list(tokenize=function(x){
-                            NGramTokenizer(x,control=Weka_control(min=1,max=1))
-                          },
-                          bounds=list(local=c(1,Inf))))
-
-gr2 <- TermDocumentMatrix(corp,control = list(tokenize=function(x){
-                                   NGramTokenizer(x,control=Weka_control(min=2,max=2))
-                                   },
-                                   bounds=list(local=c(1,Inf)),
-                                   stopwords=TRUE))
-                                   
-
-gr3 <- TermDocumentMatrix(corp,control = list(tokenize=function(x){
-                                   NGramTokenizer(x,control=Weka_control(min=3,max=3))
-                                 },
-                                 bounds=list(local=c(1,Inf))))
-
-gr4 <- TermDocumentMatrix(corp,control = list(tokenize=function(x){
-                                NGramTokenizer(x,control=Weka_control(min=4,max=4))
-                               },
-                               bounds=list(local=c(1,Inf))))
-
-gr5 <- TermDocumentMatrix(corp,control = list(tokenize=function(x){
-  NGramTokenizer(x,control=Weka_control(min=5,max=5))
-},
-bounds=list(local=c(1,Inf))))
-
-flatten <- function(tdm){
-  #summing up TermDocumentMatrices
-  rowSums(as.matrix(tdm))
-}
-
-gr <- list(gr1,gr2,gr3,gr4,gr5)
-
 library(stringr)
 library(dplyr)
 
-ngramdb <-  tibble(grams=numeric(),freq=numeric(),fourth=character(),third=character(),second=character(),first=character(),pred=character())
+#Clustering, tokenisation, handling of unknown words
+#---------------------------------------------------
+single_words <- NGramTokenizer(flatcorp,control=Weka_control(min=1,max=1))
+single_words <- tibble(feature=single_words) %>% group_by(feature) %>% summarise(freq=n())
 
-for(i in 1:5){
-  flat <- flatten(gr[[i]])
-  split=str_split_fixed(names(flat),pattern = " ",n = 5)
-  gram_tib <-  tibble(
-    grams=i,
-    freq=flat, # frequency of occurance in the given order (n in ngram)
-                      fourth=split[,c(2,3,4,5,1)[i]],
-                      third=split[,c(3,4,5,1,2)[i]], # third
-                      second=split[,c(4,5,1,2,3)[i]], # second predictor (2 words before last)
-                      first=split[,c(5,1,2,3,4)[i]], # first predictor (penultimate word)
-                      pred=split[,i] #last word - prediction
-                      )
-  ngramdb <- bind_rows(ngramdb,gram_tib)
+unks <- (single_words %>% filter(freq<3,freq>1) %>% select(feature))$feature
+#unique words do not need to be checked because only tokens with frequency >1 have been filtered
+#Handling unknown words
+#Replace all words that occur as unigram rarely by <unk>
+replace_unknowns <- function(features){
+  split <- str_split(features,pattern = "_",simplify=TRUE) 
+  indexes <- split %in% unks
+  split[indexes] <- "<unk>"
+  str_trim(apply(split, 1, paste,collapse=" "))  
 }
 
+freqs <- textstat_frequency(dfm(tokens(flatcorp,ngrams=2:5))) %>% filter(frequency>1)
+ngramdb <- tibble(feature=freqs$feature,frequency=as.integer(freqs$frequency))
+ngramdb <- ngramdb %>% transmute(
+  grams=as.integer(1+str_count(feature,pattern="_")),
+  prediction=str_extract(feature,pattern="[^_]+$"),
+  predictor=sub(feature,pattern = "_[^_]+$",replacement =""),
+  predictor=replace_unknowns(predictor),
+  frequency)
 
-saveRDS(ngramdb,"./training_set/ngramdb_big.rds")
+knowns <- unique(as.character(str_split(ngramdb$predictor," ",simplify = T)))
 
+#calculate conditional probabilities of predictor given the prediction
+totals <- ngramdb %>% group_by(predictor) %>% summarise(tot=sum(frequency))
+ngramdb <- ngramdb %>% left_join(totals) %>% mutate(condprob=frequency/tot) %>% select(-tot)
+
+
+#saveRDS(ngramdb,"./training_set/ngramdb_big.rds")
+saveRDS(ngramdb,"./training_set/ngramdb.rds")
+saveRDS(knowns,"./training_set/knowns.rds")
 #saveRDS(ngramdb,"./test_set/ngramdb.rds")
 
 
--- prediction.R
+++ prediction.R
@@ -6,112 +6,9 @@ library(dplyr)
 library(tm)
 
 ngramdb <- readRDS("./training_set/ngramdb.rds")
-ngramdb <- readRDS("./training_set/ngramdb_big.rds")
+#ngramdb <- readRDS("./training_set/ngramdb_big.rds")
 
-#Smoothing:
-#Replace all words that occur less than 4times as unigram by <unk>
-
-unks <- (ngramdb %>% filter(grams==1,freq<3) %>% select(pred))$pred
-
-ngramdb$pred <- replace(x = ngramdb$pred,list = ngramdb$pred %in% unks,"<unk>")
-ngramdb$fourth <- replace(x = ngramdb$fourth,list = ngramdb$fourth %in% unks,"<unk>")
-ngramdb$third <- replace(x = ngramdb$third,list = ngramdb$third %in% unks,"<unk>")
-ngramdb$second <- replace(x = ngramdb$second,list = ngramdb$second %in% unks,"<unk>")
-ngramdb$first <- replace(x = ngramdb$first,list = ngramdb$first %in% unks,"<unk>")
-
-ngramdb <- ngramdb %>% filter(grams>1) %>% group_by(grams,fourth,third,second,first,pred) %>% 
-  summarise(freq=sum(freq)) %>% ungroup()%>% filter(freq>1, pred!="<unk>") 
-
-knowns <- unique(c(ngramdb$fourth,ngramdb$third,ngramdb$second,ngramdb$first,ngramdb$pred))
-
-#Get number of records for n-gram groups *NOT USED SO FAR
-tot_grams <- ngramdb %>% group_by(grams) %>% summarise(sum(freq))
-
-#Add conditional probabilities
-totals <- ngramdb %>% group_by(grams,first,second,third,fourth) %>% summarise(tot=sum(freq))
-ngramdb <- ngramdb %>% left_join(totals) %>% mutate(condprob=freq/tot) %>% select(-tot)
-
-get_score <- function(input){
-  input= str_to_lower(gsub("//s"," ",input)) # lowercase and strip extra whitespace
-  input = tail(str_split(input," ")[[1]],5)
-  input=c(rep("",5-length(input)),input)
-  
-  gr5=ngramdb %>% 
-    filter(pred==input[5],grams==5,fourth==input[1],third==input[2],second==input[3],first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=sum(condprob)) %>% select(score)
-  
-  score5=0
-  if(nrow(gr5)>0){
-    score5=gr5[[1,1]]
-  }
-  
-  gr4=ngramdb %>% 
-    filter(pred==input[5],grams==4,third==input[2],second==input[3],first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=sum(condprob)) %>% select(score)
-  
-  score4=0
-  if(nrow(gr4)>0){
-    score4=gr4[[1,1]]
-  }
-  
-  gr3=ngramdb %>%
-    filter(pred==input[5],grams==3,second==input[3],first==input[4]) %>%
-    group_by(pred) %>% summarise(score=sum(condprob))%>% select(score)
-
-  score3=0
-  if(nrow(gr3)>0){
-   score3=gr3[[1,1]]
-  }
-  
-  gr2=ngramdb %>%
-    filter(pred==input[5],grams==2,first==input[4]) %>%
-    group_by(pred) %>% summarise(score=sum(condprob))%>% select(score)
-
-  score2=0
-  if(nrow(gr2)>0){
-    score2=gr2[[1,1]]
-  }
-
-  gr1=ngramdb %>%
-    filter(grams==1, pred ==input[5]) %>%
-    group_by(pred) %>% summarise(score=sum(condprob))%>% select(score)
-  
-  score1=0
-  if(nrow(gr1)>0){
-    score1=gr1[[1,1]]
-  }
-
-  c(score1,score2,score3,score4,score5)
-}
-
-dev_set_addr="./test_set/ngramdb.rds"
-dev_set<- readRDS(dev_set_addr)
-dev_set <- dev_set %>% filter(grams==5) %>% transmute(question=paste(fourth,third,second,first,pred))
-set.seed(12)
-ids_dev <- sample(1:nrow(dev_set),600)
-
-optimise_coeffs <- function(){
-  # The prediction model will be following: 
-  # P_hat(W|W-1,W-2...)=P(W|W-1,...W-k+1) + lambda*P(W|W-1...W-k).
-  # 1. Run function get_score on the development set
-  # 2. Sort the results to groups depending on the longest token found in the ngram database (k)
-  # 3. Optimise the lambda coefficients for the given groups.
-  #
-  
-  r <- sapply(FUN = get_score,X = dev_set$question[ids_dev][1:100])
-  
-  groups <- vector(mode = "numeric",length = ncol(r))
-  for (i in 1:ncol(r)){
-    for(j in 5:1){
-      if(r[j,i]!=0){
-        groups[i]=j
-        break
-      }
-    }
-  }
-  
-  r[,groups==5]
-}
+knowns <- readRDS("./training_set/knowns.rds")
 
 #Function to predict next word
 #Principle: Interpolation - word is guessed based on weighted conditional 
@@ -119,14 +16,14 @@ optimise_coeffs <- function(){
 predict_2 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
   input=stripWhitespace(removeNumbers(removePunctuation(input)))
   input= str_to_lower(gsub("^ * | $ *","",input)) # lowercase and strip extra whitespace before and after
-  
+
   input = tail(str_split(input," ")[[1]],4)
   input=replace(input,!(input %in% knowns),"<unk>")
   input=c(rep("",4-length(input)),input)
   
   gr5=ngramdb %>% 
-    filter(grams==5,fourth==input[1],third==input[2],second==input[3],first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=c5*sum(condprob))
+    filter(grams==5,predictor==paste(input,collapse=" ")) %>%# top_n(10,condprob) %>% 
+    group_by(prediction) %>% summarise(score=c5*sum(condprob))
     #ngrams are up-voted by factor c
   
   score5=0
@@ -135,8 +32,8 @@ predict_2 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
   }
   
   gr4=ngramdb %>% 
-    filter(grams==4,third==input[2],second==input[3],first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=c4*sum(condprob)) 
+    filter(grams==4,predictor==paste(input[2:4],collapse=" ")) %>% 
+    group_by(prediction) %>% summarise(score=c4*sum(condprob))
   
   score4=0
   if(nrow(gr4)>0){
@@ -144,8 +41,8 @@ predict_2 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
   }
   
   gr3=ngramdb %>% 
-    filter(grams==3,second==input[3],first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=c3*sum(condprob))
+    filter(grams==3,predictor==paste(input[3:4],collapse=" ")) %>% 
+    group_by(prediction) %>% summarise(score=c3*sum(condprob))
   
   score3=0
   if(nrow(gr3)>0){
@@ -153,21 +50,18 @@ predict_2 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
   }
   
   gr2=ngramdb %>% 
-    filter(grams==2,first==input[4]) %>% 
-    group_by(pred) %>% summarise(score=c2*sum(condprob)) 
+    filter(grams==2,predictor==paste(input[4],collapse=" ")) %>% top_n(10,condprob) %>% 
+    group_by(prediction) %>% summarise(score=c2*sum(condprob))
+  #top_n is here for performance reasons
   
   score2=0
   if(nrow(gr2)>0){
     score2=top_n(gr2,1,score)[1,2]
   }
   
-  #gr1=ngramdb %>% 
-  #  filter(grams==1, pred %in% gr2$pred) %>% 
-  #  group_by(pred) %>% summarise(score=c1*sum(condprob)) 
-  
-  pred <- (bind_rows(gr5,gr4,gr3,gr2) %>% #,gr1) %>%
-    group_by(pred) %>% summarise(score=sum(score,na.rm=TRUE)) %>% top_n(((alternatives*2)+1),score) %>% 
-    arrange(desc(score)))$pred
+  pred <- (bind_rows(gr5,gr4,gr3,gr2) %>% 
+    group_by(prediction) %>% summarise(score=sum(score,na.rm=TRUE)) %>% top_n(((alternatives*2)+1),score) %>% 
+    arrange(desc(score)))$prediction
   
   if(length(pred)<3){
     pred=head(append(pred,c('the', 'on', 'a')),3)
@@ -175,12 +69,67 @@ predict_2 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
   pred
 }
 
+predict_3 <- function(input,alternatives=T,c5=1,c4=1,c3=1,c2=1,c1=1){
+  input=stripWhitespace(removeNumbers(removePunctuation(input)))
+  input= str_to_lower(gsub("^ * | $ *","",input)) # lowercase and strip extra whitespace before and after
+  
+  input = tail(str_split(input," ")[[1]],4)
+  input=replace(input,!(input %in% knowns),"<unk>")
+  input=c(rep("",4-length(input)),input)
+  
+  gr5=ngramdb %>% 
+    filter(grams==5,predictor==paste(input,collapse=" ")) 
+  #ngrams are up-voted by factor c
+  
+  score5=0
+  if(nrow(gr5)>0){
+    score5=top_n(gr5,1,condprob)[1,2]
+  }
+  
+  gr4=ngramdb %>% 
+    filter(grams==4,predictor==paste(input[2:4],collapse=" ")) 
+  
+  score4=0
+  if(nrow(gr4)>0){
+    score4=top_n(gr4,1,condprob)[1,2]
+  }
+  
+  gr3=ngramdb %>% 
+    filter(grams==3,predictor==paste(input[3:4],collapse=" "))
+  
+  score3=0
+  if(nrow(gr3)>0){
+    score3=top_n(gr3,1,condprob)[1,2]
+  }
+  
+  gr2=ngramdb %>% 
+    filter(grams==2,predictor==paste(input[4],collapse=" "))
+  #top_n is here for performance reasons
+  
+  score2=0
+  if(nrow(gr2)>0){
+    score2=top_n(gr2,1,condprob)[1,2]
+  }
+  
+  pred <- (bind_rows(gr5,gr4,gr3,gr2) %>% 
+             group_by(prediction) %>% summarise(score=sum(condprob,na.rm=TRUE)) %>% top_n(((alternatives*4)+1),score) %>% 
+             arrange(desc(score)))$prediction
+  
+  if(length(pred)<5){
+    pred=head(append(pred,c('the', 'on', 'a','at','on')),5)
+  }
+  pred
+}
+
+
+
 test_set="./test_set/ngramdb.rds"
 yy<- readRDS(test_set)
 yy <- yy %>% filter(grams==5) %>% transmute(question=paste(fourth,third,second,first),correct=pred)
 ids <- sample(1:nrow(yy),500)
 
 test <- function(k1,k2,k3,k4,k5){
+  time1 <- Sys.time()
 
   #ids <- 1:length(yy) 
   #######################
@@ -192,14 +141,30 @@ test <- function(k1,k2,k3,k4,k5){
   answers=c()
   answers2=c()
   answers3=c()
+  answers4=c()
+  answers5=c()
   for (i in 1:length(questions)){
-    res=predict_2(questions[i],T,c1=k1,c2=k2,c3=k3,c4=k4,c5=k5)
+    res=predict_3(questions[i],T,c1=k1,c2=k2,c3=k3,c4=k4,c5=k5)
     answers=append(answers,res[[1]])
     answers2=append(answers2,res[[2]])
     answers3=append(answers3,res[[3]])
+    answers4=append(answers4,res[[4]])
+    answers5=append(answers5,res[[5]])
   }
-  protocoll=data.frame(questions,answers,correct,answers2,answers3,stringsAsFactors = F) %>% mutate(match=(answers==correct))
-  print(protocoll %>% summarise(accuracy=mean(match)))
+  protocoll=data.frame(questions,answers,correct,answers2,answers3,stringsAsFactors = F) %>% 
+    mutate(match=(answers==correct),
+           match2=(answers==correct | answers2==correct),
+           match3=(answers==correct | answers2==correct| answers3==correct),
+           match4=(answers==correct | answers2==correct| answers3==correct| answers4==correct),
+           match5=(answers==correct | answers2==correct| answers3==correct| answers4==correct| answers5==correct))
+  x <- (protocoll %>% 
+          summarise(accuracy1=mean(match),accuracy2=mean(match2),
+                    accuracy3=mean(match3),accuracy4=mean(match4),
+                    accuracy5=mean(match5)))
+  print(x)
+  plot(t(x[1,]),ylim = c(0,1))
+  time2 <- Sys.time()
+  print((time2-time1)/length(ids))
   protocoll
 }
 
